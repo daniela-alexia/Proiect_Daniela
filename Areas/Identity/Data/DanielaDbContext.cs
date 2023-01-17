be89eb44028f3df78305d9ff7ce005be0a04545a
IuliaDragota <iulia.dragota@stud.ubbcluj.ro>
2023-01-17 13:33:00 +0200

    login functional

---
+++ Areas/Identity/Data/DanielaDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Proiect_Daniela.Models.Domain;
+
+namespace Proiect_Daniela.Data
+{
+    public class DanielaDbContext : IdentityDbContext<IdentityUser>
+    {
+    
+        public DanielaDbContext(DbContextOptions<DanielaDbContext> options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        // utilizat de migrari ca sa construiasca un nou tabel
+        // in baza de date in caz ca nu exista
+        public DbSet <Angajat> Angajati { get; set; }
+    }
+}
